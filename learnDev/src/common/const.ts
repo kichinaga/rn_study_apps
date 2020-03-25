dd427ca067c4374ee5d35b1cdc9f571675697888
Takumi Yoshinaga <takumi.yoshinaga@theport.jp>
2020-03-25 18:32:16 +0900

    add handing text input + α
    - reverse a string when submit input text

--- learnDev/src/common/const.ts
+++ learnDev/src/common/const.ts
@@ -2,10 +2,12 @@ import RootNavigator from '../navigator/RootNavigator';
 import HomeScreen from '../ui/nav_sample/HomeScreen';
 import PropsComponent from '../ui/props/PropsComponent';
 import StateComponent from '../ui/state/StateComponent';
+import TextInputComponent from '../ui/text_input/TextInputComponent';
 
 export const Routings = [
   { title: 'root', component: RootNavigator },
   { title: 'home', component: HomeScreen },
   { title: 'props', component: PropsComponent },
   { title: 'state', component: StateComponent },
+  { title: 'textInput', component: TextInputComponent },
 ]
